bf794a52278b0c0b349bbe83f110c9bbb8fdfb7a
Thinkman Wang <wangxf1985@gmail.com>
2021-12-06 22:52:32 +0800

    update

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"GOThinkUtils/thinkutils"
+	"fmt"
+	"log"
+	"strconv"
 )
 
-func main() {
-	fmt.Println("Hello World")
-
+func datetimeTest() {
 	fmt.Println(thinkutils.DateTime.Timestamp())
 	fmt.Println(thinkutils.DateTime.TimestampMs())
 	fmt.Println(thinkutils.StringUtils.IsEmpty(" 123 "))
@@ -17,4 +16,26 @@ func main() {
 	*pszTxt = " 123"
 	fmt.Println(thinkutils.StringUtils.IsEmptyPtr(pszTxt))
 	fmt.Println(thinkutils.StringUtils.IsEmpty(*pszTxt))
+
+	fmt.Println(thinkutils.DateTime.CurDatetime())
+	fmt.Println(thinkutils.DateTime.Yesterday())
+	fmt.Println(thinkutils.DateTime.Tomorrow())
+	fmt.Println(thinkutils.DateTime.TimeStampToDateTime(thinkutils.DateTime.Timestamp()))
+	fmt.Println(thinkutils.DateTime.Hour())
+	fmt.Println(strconv.Atoi("05"))
+
+	fmt.Println(thinkutils.DateTime.DiffDate(-3))
+	fmt.Println(thinkutils.DateTime.DiffDate(4))
+	fmt.Println(thinkutils.DateTime.DateToTimestamp("2021-12-06"))
+	fmt.Println(thinkutils.DateTime.FirstDayOfMonth("2021-10-20"))
+	fmt.Println(thinkutils.DateTime.LastDayOfMonth("2021-03-01"))
+}
+
+func main() {
+	fmt.Println("Hello World")
+
+	//var logger *log.Logger = new(log.Logger)
+	log.Println("FXXK")
+
+	datetimeTest()
 }
--- thinkutils/dateutils.go
+++ thinkutils/dateutils.go
@@ -1,6 +1,9 @@
 package thinkutils
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type datetime struct {
 }
@@ -13,4 +16,91 @@ func (this datetime) Timestamp() int64 {
 func (this datetime) TimestampMs() int64 {
 	now := time.Now()
 	return now.UnixMilli()
+}
+
+func (this datetime) CurDatetime() string {
+	t := time.Now()
+	return t.Format("2006-01-02 15:04:05")
+}
+
+func (this datetime) Hour() int {
+	t := time.Now()
+
+	nRet, err := strconv.Atoi(t.Format("15"))
+	if err != nil {
+		return 0
+	}
+
+	return nRet
+}
+
+func (this datetime) Yesterday() string {
+	return this.DiffDate(-1)
+}
+
+func (this datetime) Tomorrow() string {
+	return this.DiffDate(1)
+}
+
+func (this datetime) DiffDate(nDay int) string {
+	var nTimestamp = this.Timestamp()
+	nTimestamp += 3600 * 24 * int64(nDay)
+	t := time.Unix(nTimestamp, 0)
+
+	return t.Format("2006-01-02")
+}
+
+func (this datetime) TimeStampToDate(nTimestamp int64) string {
+	t := time.Unix(nTimestamp, 0)
+
+	return t.Format("2006-01-02")
+}
+
+func (this datetime) DateToTimestamp(szDate string) int64 {
+	t, err := time.Parse("2006-01-02", szDate)
+	if err != nil {
+		return 0
+	}
+
+	return t.Unix()
+}
+
+func (this datetime) DateTimeToTimestamp(szDate string) int64 {
+	t, err := time.Parse("2006-01-02 15:04:05", szDate)
+	if err != nil {
+		return 0
+	}
+
+	return t.Unix()
+}
+
+func (this datetime) TimeStampToDateTime(nTimestamp int64) string {
+	t := time.Unix(nTimestamp, 0)
+
+	return t.Format("2006-01-02 15:04:05")
+}
+
+func (this datetime) FirstDayOfMonth(szDate string) string {
+	t, err := time.Parse("2006-01-02", szDate)
+	if err != nil {
+		return ""
+	}
+
+	strMonth := t.Format("2006-01")
+
+	return strMonth + "-01"
+}
+
+func (this datetime) LastDayOfMonth(szDate string) string {
+	szFirst := this.FirstDayOfMonth(szDate)
+	nTimestamp := this.DateToTimestamp(szFirst)
+	nTimestamp += 32 * 3600 * 24
+
+	szNextMonth := this.TimeStampToDate(nTimestamp)
+	szNextFirst := this.FirstDayOfMonth(szNextMonth)
+
+	nTimestamp = this.DateToTimestamp(szNextFirst)
+	nTimestamp -= 24 * 3600
+
+	return this.TimeStampToDate(nTimestamp)
 }
