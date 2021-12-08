fa35d6d1dbf154480a99479a5c749390f87034df
Thinkman Wang <wangxf1985@gmail.com>
2021-12-08 09:48:38 +0800

    update

--- thinkutils/test/mysql/mysqltest.go
+++ thinkutils/test/mysql/mysqltest.go
@@ -4,13 +4,14 @@ import (
 	"GOThinkUtils/thinkutils"
 	"GOThinkUtils/thinkutils/logger"
 	"fmt"
+	"sync"
 )
 
 var (
 	log *logger.LocalLogger = logger.DefaultLogger()
 )
 
-func basicQueryJSON() {
+func basicQueryJSON(wg *sync.WaitGroup) {
 	db := thinkutils.ThinkMysql.QuickConn()
 
 	rows, err := db.Query("SELECT * FROM sys_user")
@@ -20,12 +21,18 @@ func basicQueryJSON() {
 
 	szRet := thinkutils.ThinkMysql.ToJSON(rows)
 	fmt.Println(szRet)
+	wg.Done()
 	defer rows.Close()
 }
 
 func main() {
+	wg := sync.WaitGroup{}
 	for i := 0; i < 100; i++ {
-		basicQueryJSON()
+		wg.Add(1)
+		go basicQueryJSON(&wg)
 	}
+
+	wg.Wait()
+	//time.Sleep(10 * time.Second)
 	//basicQueryJSON()
 }
