538e952eae78dd0d13894c01519661b7850a992f
Thinkman Wang <wangxf1985@gmail.com>
2021-12-08 10:49:52 +0800

    update

--- thinkutils/test/mysql/mysqltest.go
+++ thinkutils/test/mysql/mysqltest.go
@@ -11,10 +11,31 @@ var (
 	log *logger.LocalLogger = logger.DefaultLogger()
 )
 
+/*
+	sqlStatement := `
+	UPDATE users
+	SET first_name = $2, last_name = $3
+	WHERE id = $1;`
+	res, err := db.Exec(sqlStatement, 5, "NewFirst", "NewLast")
+	if err != nil {
+	  panic(err)
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+	  panic(err)
+	}
+	fmt.Println(count)
+*/
 func basicQueryJSON(wg *sync.WaitGroup) {
 	db := thinkutils.ThinkMysql.QuickConn()
 
-	rows, err := db.Query("SELECT * FROM sys_user WHERE user_id > ?", 0)
+	rows, err := db.Query(`
+		SELECT 
+       		* 
+		FROM 
+		    sys_user 
+		WHERE 
+		    user_id > ?`, 0)
 	if err != nil {
 		return
 	}
