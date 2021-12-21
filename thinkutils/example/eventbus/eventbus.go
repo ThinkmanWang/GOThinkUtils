535a550303186c7788659fc17176fd782b04dc3c
Thinkman Wang <wangxf1985@gmail.com>
2021-12-21 17:53:40 +0800

    update

--- thinkutils/example/eventbus/eventbus.go
+++ thinkutils/example/eventbus/eventbus.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"GOThinkUtils/thinkutils/logger"
+	"fmt"
 	"github.com/asaskevich/EventBus"
 	"sync"
+	"time"
 )
 
 var (
@@ -12,15 +14,35 @@ var (
 )
 
 func onMsg(szMsg string) {
+	time.Sleep(5 * time.Second)
 	log.Info(szMsg)
+}
+
+type MyStruct struct {
+	Name string
+	Desc string
+}
+
+func onStructMsg(data MyStruct) {
+	time.Sleep(5 * time.Second)
+	fmt.Println(data)
+}
+
+func onMultiParams(szName string, szDesc string) {
+	time.Sleep(5 * time.Second)
+	log.Info("%s %s", szName, szDesc)
 }
 
 func publish() {
 	bus.Publish("main:message", "FXXK")
+	bus.Publish("main:otherMessage", MyStruct{Name: "123", Desc: "456"})
+	bus.Publish("main:multiParams", "123", "456")
 }
 
 func main() {
-	bus.Subscribe("main:message", onMsg)
+	bus.SubscribeAsync("main:message", onMsg, false)
+	bus.SubscribeAsync("main:otherMessage", onStructMsg, false)
+	bus.SubscribeAsync("main:multiParams", onMultiParams, false)
 
 	go func() {
 		publish()
