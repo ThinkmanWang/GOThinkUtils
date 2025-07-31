3095155196d6f9ea8f7ec06d6538e7b65e3d1614
ThinkmanWang <wangxf1985@gmail.com>
2025-07-31 22:16:51 +0800

    add Eventbus

--- thinkutils/thinkutils.go
+++ thinkutils/thinkutils.go
@@ -4,28 +4,30 @@ import (
 	"errors"
 	"fmt"
 	"github.com/ThinkmanWang/GOThinkUtils/thinkutils/logger"
+	"github.com/asaskevich/EventBus"
 )
 
 var (
 	log *logger.LocalLogger = logger.DefaultLogger()
 
-	DateTime     datetime
-	StringUtils  stringutils
-	RandUtils    randutils
-	MD5Utils     md5utils
-	IPUtils      iputils
-	UUIDUtils    uuidutils
-	ThinkMysql   thinkmysql
-	ThinkRedis   thinkredis
-	FileUtils    fileutils
-	RegularUtils regulartils
-	JSONUtils    jsonutils
-	KafkaUtils   kafkautils
-	UDPUtils     udputils
-	Base64Utils  base64utils
-	HttpUtils    httputils
-	SetUtils     setutils
-	StructUtils  structutis
+	DateTime      datetime
+	StringUtils   stringutils
+	RandUtils     randutils
+	MD5Utils      md5utils
+	IPUtils       iputils
+	UUIDUtils     uuidutils
+	ThinkMysql    thinkmysql
+	ThinkRedis    thinkredis
+	FileUtils     fileutils
+	RegularUtils  regulartils
+	JSONUtils     jsonutils
+	KafkaUtils    kafkautils
+	UDPUtils      udputils
+	Base64Utils   base64utils
+	HttpUtils     httputils
+	SetUtils      setutils
+	StructUtils   structutis
+	ThinkEventBus EventBus.Bus = EventBus.New()
 )
 
 func ListRemoveAt[T int8 | int16 | int32 | int64 | int | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | string | any](lstData []T, nIndex int) []T {
