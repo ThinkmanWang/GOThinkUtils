e28da5175903ef6a276668b0c07e5dbbf112a6f2
Thinkman Wang <wangxf1985@gmail.com>
2026-07-22 19:31:21 +0800

    fxxk

--- go.mod
+++ go.mod
@@ -20,17 +20,19 @@ require (
 )
 
 require (
+	github.com/allegro/bigcache/v3 v3.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-co-op/gocron v1.37.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/google/uuid v1.1.1 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/klauspost/cpuid v1.3.1 // indirect
@@ -42,10 +44,12 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/rs/xid v1.2.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	github.com/yinheli/mahonia v0.0.0-20131226213531-0eef680515cc // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
 	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
---
+++ thinkutils/ThinkBigCache.go
@@ -0,0 +1,234 @@
+package thinkutils
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/allegro/bigcache/v3"
+	"github.com/go-co-op/gocron"
+)
+
+type OnThinkBigCacheUpdate func()
+
+type ThinkBigCacheUpdateType int
+
+const (
+	UPDATE_1_MIN ThinkBigCacheUpdateType = iota
+	UPDATE_5_MIN
+	UPDATE_10_MIN
+	UPDATE_30_MIN
+	UPDATE_1_HOUR
+	UPDATE_6_HOUR
+	UPDATE_12_HOUR
+	UPDATE_1_DAY
+)
+
+type ThinkBigCache struct {
+	m_lock      sync.Mutex
+	m_bStarted  bool
+	m_pCronJobs *gocron.Scheduler
+	m_pBigCache *bigcache.BigCache
+
+	m_lst1MinListener   []OnThinkBigCacheUpdate
+	m_lst5MinListener   []OnThinkBigCacheUpdate
+	m_lst10MinListener  []OnThinkBigCacheUpdate
+	m_lst30MinListener  []OnThinkBigCacheUpdate
+	m_lst1HourListener  []OnThinkBigCacheUpdate
+	m_lst6HourListener  []OnThinkBigCacheUpdate
+	m_lst12HourListener []OnThinkBigCacheUpdate
+	m_lst1DayListener   []OnThinkBigCacheUpdate
+}
+
+var (
+	g_pThinkBigCacheInstance *ThinkBigCache = &ThinkBigCache{
+		m_bStarted: false,
+	}
+)
+
+func ThinkBigCacheInstance() *ThinkBigCache {
+	return g_pThinkBigCacheInstance
+}
+
+func (this *ThinkBigCache) loadFromDisk(c *bigcache.BigCache, path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	return gob.NewDecoder(bytes.NewBuffer(data)).Decode(c)
+}
+
+func (this *ThinkBigCache) saveToDisk(c *bigcache.BigCache, path string) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(c); err != nil {
+		return err
+	}
+
+	tmp := path + ".tmp"
+	if err := os.WriteFile(tmp, buf.Bytes(), 0644); err != nil {
+		return err
+	}
+	return os.Rename(tmp, path)
+}
+
+func (this *ThinkBigCache) emitUpdate(nType ThinkBigCacheUpdateType) {
+	switch nType {
+	case UPDATE_1_MIN:
+		for _, pFunc := range this.m_lst1MinListener {
+			go pFunc()
+		}
+	case UPDATE_5_MIN:
+		for _, pFunc := range this.m_lst5MinListener {
+			go pFunc()
+		}
+	case UPDATE_10_MIN:
+		for _, pFunc := range this.m_lst10MinListener {
+			go pFunc()
+		}
+	case UPDATE_30_MIN:
+		for _, pFunc := range this.m_lst30MinListener {
+			go pFunc()
+		}
+	case UPDATE_1_HOUR:
+		for _, pFunc := range this.m_lst1HourListener {
+			go pFunc()
+		}
+	case UPDATE_6_HOUR:
+		for _, pFunc := range this.m_lst6HourListener {
+			go pFunc()
+		}
+	case UPDATE_12_HOUR:
+		for _, pFunc := range this.m_lst12HourListener {
+			go pFunc()
+		}
+	case UPDATE_1_DAY:
+		for _, pFunc := range this.m_lst1DayListener {
+			go pFunc()
+		}
+	}
+}
+
+func (this *ThinkBigCache) initCron() error {
+	this.m_pCronJobs = gocron.NewScheduler(time.Local)
+
+	this.m_lst1MinListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst5MinListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst10MinListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst30MinListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst1HourListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst6HourListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst12HourListener = make([]OnThinkBigCacheUpdate, 0)
+	this.m_lst1DayListener = make([]OnThinkBigCacheUpdate, 0)
+
+	_, _ = this.m_pCronJobs.Cron("* * * * *").Do(func() {
+		this.emitUpdate(UPDATE_1_MIN)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 */5 * * * *").Do(func() {
+		this.saveToDisk(this.m_pBigCache, "ThinkBigCache.data")
+		this.emitUpdate(UPDATE_5_MIN)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 */10 * * * *").Do(func() {
+		this.emitUpdate(UPDATE_10_MIN)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 */30 * * * *").Do(func() {
+		this.emitUpdate(UPDATE_30_MIN)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 0 * * * *").Do(func() {
+		this.emitUpdate(UPDATE_1_HOUR)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 0 0,6,12,18 * * *").Do(func() {
+		this.emitUpdate(UPDATE_6_HOUR)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 0 0,12 * * *").Do(func() {
+		this.emitUpdate(UPDATE_12_HOUR)
+	})
+
+	_, _ = this.m_pCronJobs.Cron("0 0 0 * * *").Do(func() {
+		this.emitUpdate(UPDATE_12_HOUR)
+	})
+
+	return nil
+}
+
+func (this *ThinkBigCache) Start() error {
+	this.m_lock.Lock()
+	defer this.m_lock.Unlock()
+
+	if this.m_bStarted {
+		return nil
+	}
+
+	var err error = nil
+
+	cfg := bigcache.Config{
+		Shards:           1024,
+		LifeWindow:       100 * 365 * 24 * time.Hour, // 逻辑永久有效
+		CleanWindow:      0,                          // 关闭后台清理，库绝不主动删除数据
+		MaxEntrySize:     4 * 1024 * 1024,
+		HardMaxCacheSize: 0, // 无内存上限
+		Verbose:          false,
+	}
+
+	this.m_pBigCache, _ = bigcache.New(context.Background(), cfg)
+	err = this.loadFromDisk(this.m_pBigCache, "ThinkBigCache.data")
+	if err != nil {
+		goto err_ret
+	}
+
+	err = this.initCron()
+	if err != nil {
+		goto err_ret
+	}
+
+	this.m_bStarted = true
+
+err_ret:
+	return err
+}
+
+func (this *ThinkBigCache) AddUpdateListener(nType ThinkBigCacheUpdateType, pFunc OnThinkBigCacheUpdate) {
+	switch nType {
+	case UPDATE_1_MIN:
+		this.m_lst1MinListener = append(this.m_lst1MinListener, pFunc)
+	case UPDATE_5_MIN:
+		this.m_lst5MinListener = append(this.m_lst5MinListener, pFunc)
+	case UPDATE_10_MIN:
+		this.m_lst10MinListener = append(this.m_lst10MinListener, pFunc)
+	case UPDATE_30_MIN:
+		this.m_lst30MinListener = append(this.m_lst30MinListener, pFunc)
+	case UPDATE_1_HOUR:
+		this.m_lst1HourListener = append(this.m_lst1HourListener, pFunc)
+	case UPDATE_6_HOUR:
+		this.m_lst6HourListener = append(this.m_lst6HourListener, pFunc)
+	case UPDATE_12_HOUR:
+		this.m_lst12HourListener = append(this.m_lst12HourListener, pFunc)
+	case UPDATE_1_DAY:
+		this.m_lst1DayListener = append(this.m_lst1DayListener, pFunc)
+	}
+}
+
+func (this *ThinkBigCache) Set(szKey string, data []byte) error {
+	return this.m_pBigCache.Set(szKey, data)
+}
+
+func (this *ThinkBigCache) Get(szKey string) ([]byte, error) {
+	if data, err := this.m_pBigCache.Get(szKey); err != nil {
+		return nil, err
+	} else {
+		return data, nil
+	}
+}
